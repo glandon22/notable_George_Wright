d06d8bcdfddc44f8cd420c0d2e85e3be3c91ed87
glandon22 <glandonwright@gmail.com>
2020-06-14 14:28:29 -0400

    Adding ability to creat new appointment for existing doctor

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ let appointments = {
         "09222020": {
             "0900": [
                 {
-                    "id": "321-4657-54323",
+                    "id": "12",
                     "firstName": "George",
                     "lastName": "Wright",
                     "type": "New Patient"
@@ -79,6 +79,55 @@ app.get('/appointments', function(req, res) {
     res.status(200).send(JSON.stringify(appointments[doctor][date]));
 });
 
+app.post('/appointment', function(req, res) {
+    const doctor = req.body.doctorID;
+    const appointment = req.body.appointment;
+    const date = appointment.date;
+    if (!doctor || !appointments[doctor]) {
+        res.status(400).send("Please include a valid doctorID with the request.");
+        return;
+    }
+    else if (parseInt(appointment.time) % 15 !== 0) {
+        res.status(400).send("Please include a valid appointment time. Appointment times begin at 9 AM in 15 minute increments, ex. 0915.");
+        return;
+    }
+    else if (!appointment.firstName) {
+        res.status(400).send("Please include a valid patient first name.");
+        return;
+    }
+    else if (!appointment.lastName) {
+        res.status(400).send("Please include a valid patient last name.");
+        return;
+    }
+    else if (appointment.type != "New Patient" && appointment.type != "Follow-Up") {
+        res.status(400).send("Please include a valid patient type, New Patient or Follow-Up.");
+        return;
+    }
+
+    if (!appointments[doctor][date]) {
+        appointments[doctor][date] = {};
+    }
+    
+    if (!appointments[doctor][date][appointment.time]) {
+        appointments[doctor][date][appointment.time] = [];
+    }      
+    
+    if (appointments[doctor][date][appointment.time].length >= 3) {
+        res.status(400).send("This doctor already has three appoints for this date and time."); 
+        return;
+    }
+
+    appointments[doctor][date][appointment.time].push({
+        "id": appointment.id,
+        "firstName": appointment.firstName,
+        "lastName": appointment.lastName,
+        "type": appointment.type
+    });
+
+    res.status(200).send("Appointment created successfully");
+    console.log(JSON.stringify(appointments, null, 4));
+});
+
 app.listen(PORT, function () {
     console.log(`Listening on port ${PORT}`);
-  });
+});
